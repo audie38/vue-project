c335f7ae791dcd0011e3b09087433e266414c06f
Audie Milson <milson.audie@gmail.com>
2023-04-23 18:06:23 +0700

    Implement Coach Filter

--- cli/coach-finder/src/components/coaches/CoachFilter.vue
+++ cli/coach-finder/src/components/coaches/CoachFilter.vue
@@ -1,11 +1,39 @@
 <template>
-  <base-card></base-card>
+  <base-card>
+    <div class="p-3">
+      <h2>Find Your Coach</h2>
+      <div class="d-flex justify-content-start align-items-center">
+        <div class="me-2">
+          <input class="me-1" value="frontend" type="checkbox" id="frontend" checked v-model="checkedAreas" @change="setFilter" />
+          <label for="frontend">Frontend</label>
+        </div>
+        <div class="me-2">
+          <input class="me-1" value="backend" type="checkbox" id="backend" checked v-model="checkedAreas" @change="setFilter" />
+          <label for="backend">Backend</label>
+        </div>
+        <div class="me-2">
+          <input class="me-1" value="career" type="checkbox" id="career" checked v-model="checkedAreas" @change="setFilter" />
+          <label for="career">Career</label>
+        </div>
+      </div>
+    </div>
+  </base-card>
 </template>
 
 <script>
 export default {
   name: "CoachFilter",
   components: {},
+  data() {
+    return {
+      checkedAreas: ["frontend", "backend", "career"],
+    };
+  },
+  methods: {
+    setFilter() {
+      this.$emit("change-filter", this.checkedAreas);
+    },
+  },
 };
 </script>
 
--- cli/coach-finder/src/pages/coaches/CoachesList.vue
+++ cli/coach-finder/src/pages/coaches/CoachesList.vue
@@ -1,12 +1,12 @@
 <template>
-  <CoachFilter />
+  <CoachFilter @change-filter="setFilter" />
   <base-card>
     <div class="d-flex justify-content-start align-items-center mx-3">
       <button class="btn btn-outline-dark me-1">Refresh</button>
       <router-link class="btn btn-dark ms-auto" to="/register">Register as Coach</router-link>
     </div>
     <div v-if="hasCoaches">
-      <CoachItem v-for="coach in coaches" :key="coach.id" :coachData="coach" />
+      <CoachItem v-for="coach in filteredCoaches" :key="coach.id" :coachData="coach" />
     </div>
     <div class="mt-3" v-else>
       <h3>No Coaches found</h3>
@@ -26,8 +26,26 @@ export default {
     CoachFilter,
     CoachItem,
   },
+  data() {
+    return {
+      areaFilters: ["frontend", "backend", "career"],
+    };
+  },
   computed: {
-    ...mapGetters("coach", ["coaches", "hasCoaches"]),
+    ...mapGetters("coach", ["hasCoaches"]),
+    filteredCoaches() {
+      const coaches = this.$store.getters["coach/coaches"];
+      return coaches.filter((coach) => {
+        if (this.areaFilters.some((area) => coach.areas.includes(area))) {
+          return true;
+        }
+      });
+    },
+  },
+  methods: {
+    setFilter(filter) {
+      this.areaFilters = filter;
+    },
   },
 };
 </script>
