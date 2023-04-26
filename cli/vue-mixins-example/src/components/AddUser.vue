ed6c09c0ed986346424a08864f8b00098c1a4cdd
Audie Milson <milson.audie@gmail.com>
2023-04-26 21:14:14 +0700

    Implement Mixins and Global Mixins

---
+++ cli/vue-mixins-example/src/components/AddUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script>
+import UserAlert from "./UserAlert.vue";
+import alertMixin from "@/mixins/alert.js";
+
+export default {
+  components: {
+    UserAlert,
+  },
+  data() {
+    return {
+      alertTitle: "Add User",
+    };
+  },
+  mixins: [alertMixin],
+};
+</script>
